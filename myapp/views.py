f4b0c950f539405075bed6694c82d82d2af745ed
developerMaruya <errbmaurya@gmail.com>
2023-06-26 13:03:35 +0530

    update use login and registration and logut add

--- myapp/views.py
+++ myapp/views.py
@@ -5,8 +5,15 @@ from .forms import *
 from .models import *
 from django.views import View
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
+# without login page not open so 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class HomeView(View):
     def get(self, request):
         form = ResumeForm()
@@ -19,7 +26,40 @@ class HomeView(View):
             form.save()
             return HttpResponseRedirect(request.path_info)
         return render(request, 'myapp/home.html', {'form': form})
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CandidateView(View):
    def get(self,request,pk):
       candidate=Resume.objects.get(pk=pk)
       return render(request,'myapp/candidate.html',{'candidate':candidate})
+
+
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        if pass1!=pass2:
+            return HttpResponse("Your password and Conform password are not Same")
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+    return render(request,'myapp/signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("Username and password is incorrect !!")
+    return render(request,'myapp/login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
--- resumeuploader/urls.py
+++ resumeuploader/urls.py
@@ -5,10 +5,12 @@ from myapp import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.HomeView.as_view(), name='home'),
+    path('home/',views.HomeView.as_view(), name='home'),
     path('<int:pk>',views.CandidateView.as_view(), name='candidate'),
-
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('logout/',views.LogoutPage,name='logout'),
+    # path('home/',views.Home,name='home')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
